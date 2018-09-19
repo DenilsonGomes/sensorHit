dd24bca6f677b2ad3699bee003d074e1c7104032
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-19 19:07:45 -0300

    Projeto Iniciado

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*Autor: Denilson Gomes Vaz da Silva
+* Graduando em Engenharia de Computação
+* Universidade Federal do Ceará - UFC
+*
+* sensorHit:
+* Código para verificar valor do sensorHit
+*/
+
+#include <Arduino.h>
+
+//Variaveis e Constantes
+int led = 3;
+int sensor = 2;
+int valor;
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
